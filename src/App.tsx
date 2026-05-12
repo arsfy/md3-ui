aa048c4a81255a8eeddc92de1796a09c2151d163
Arsfy <arsfy@outlook.com>
2026-05-12 19:01:07 +0800

    save

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+  return (
+    <div>
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Home</h1>
+      <p>Welcome to your React + Vite + React Router app!</p>
+    </div>
+  )
+}
+
+export default Home
